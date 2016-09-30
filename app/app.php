5a3e851c7cd88eb8d73b6421c26d3f900a0286f2
Ryan Apking <ryan.apking@gmail.com>
2016-09-30 10:46:19 -0700

    repeat last commit from the correct path.

--- app/app.php
+++ app/app.php
@@ -18,7 +18,82 @@
 
 // home
     $app->get('/', function() use ($app) {
-        return 'test';
+        return $app['twig']->render('home.html.twig');
+    });
+
+// stores routes
+    $app->get('/stores', function() use ($app) {
+        // displays a list of all stores and a form allowing for a new store to be entered
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
+
+    $app->post('/stores', function() use ($ap) {
+        // adds the new store to the database and redirects to stores get route
+        $store_name = $_POST('store_name');
+        $new_store = new Store($store_name);
+        $new_store->save();
+        return $app->redirect('/stores');
+    });
+
+// store (singular) routes
+    $app->get('/store/{store_id}', function($store_id) use ($app) {
+        // displays all brands sold in a specific store and a form allowing for a brand to be added to the store
+        $store = Store::findById($store_id);
+        $store_brands = $store->getBrandList();
+        $all_brands = Brand::getAll();
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'store_brands' => $store_brands, 'all_brands' => $all_brands));
+    });
+
+    $app->post('/store/{store_id}', function($store_id) use ($app) {
+        // adds the brand to the store, then redirects to the get route
+        $store = Store::findById($store_id);
+        $brand_id = $_POST['brand_id'];
+        $brand = Brand::findById($brand_id);
+        $store->addBrand($brand);
+        return $app->redirect('/store/' . $store_id);
+    });
+
+    $app->patch('/store/{store_id}', function($store_id) use ($app) {
+        // updates the store information, then redirects to the get route
+        $store = Store::findById($store_id);
+        $new_name = $_POST['store_name'];
+        $store->setName($new_name);
+        $store->update();
+        return $app->redirect('/store/' . $store_id);
+    });
+
+    $app->delete('/store/{store_id}', function($store_id) use ($app) {
+        // deletes the store from the database, then redirects to the stores page
+        $store = Store::findById($store_id);
+        $store->delete();
+        return $app->redirect('/stores');
+    });
+
+// brands routes
+    $app->get('/brands', function() use ($app) {
+
+    });
+
+    $app->post('/brands', function() use ($ap) {
+
+    });
+
+// brand (singular) routes
+
+    $app->get('/brand/{brand_id}', function($brand_id) use ($app) {
+
+    });
+
+    $app->post('/brand/{brand_id}', function($brand_id) use ($app) {
+
+    });
+
+    $app->patch('/brand/{brand_id}', function($brand_id) use ($app) {
+
+    });
+
+    $app->delete('/brand/{brand_id}', function($brand_id) use ($app) {
+
     });
 
     return $app;
